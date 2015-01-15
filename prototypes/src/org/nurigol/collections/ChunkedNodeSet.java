c872f60e4794123ac53a9bdc8f2400d0b0d0cf12
SungJu Cho <sungjuc73@gmail.com>
2015-01-15 02:04:57 -0800

    Chunk implementataion.

--- prototypes/src/org/nurigol/collections/ChunkedNodeSet.java
+++ prototypes/src/org/nurigol/collections/ChunkedNodeSet.java
@@ -1,75 +1,66 @@
 package org.nurigol.collections;
 
-import jdk.jfr.events.ExceptionThrownEvent;
-
 import java.nio.ByteBuffer;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.SortedSet;
 
-public class ChunkedNodeSet<T> {
-    private ByteBuffer block;
-    private int position = -1;
-    private SortedSet<T> memorySet;
+public class ChunkedNodeSet<T extends Comparable<T>> {
+	private ByteBuffer block;
+	private int position = -1;
+	private SortedSet<T> memorySet;
 
-    private int numChunks;
+	private int numChunks;
 
-    public ChunkedNodeSet(ByteBuffer block) {
-        this.block = block;
-    }
+	public ChunkedNodeSet(ByteBuffer block) {
+		this.block = block;
+	}
 
-    public ChunkedNodeSet(ByteBuffer block, int position) {
-        this.block = block;
-        this.position = position;
-    }
+	public ChunkedNodeSet(ByteBuffer block, int position) {
+		this.block = block;
+		this.position = position;
+	}
 
-    public ChunkedNodeSet(ByteBuffer block, SortedSet<T> memorySet) {
-        this.block = block;
-        this.memorySet = memorySet;
-    }
+	public ChunkedNodeSet(ByteBuffer block, SortedSet<T> memorySet) {
+		this.block = block;
+		this.memorySet = memorySet;
+	}
 
+	/**
+	 * Serializes contents in position.
+	 * 
+	 * @return
+	 */
+	public int serialize() throws Exception {
+		if (position < 0)
+			throw new Exception("ChunkedNodeList is not properly initialized.");
 
-    /**
-     * Serializes contents in position.
-     * @return
-     */
-    public int serialize() throws Exception {
-        if (position < 0)
-            throw new Exception("ChunkedNodeList is not properly initialized.");
+		// Compute chunking layout.
+		Set<Chunk<T>> chunkList = computeChunk();
 
-        // Compute chunking layout.
-        List<Chunk> chunkList = computeChunk();
+		// Serialize header information
 
-        // Serialize header information
+		// Serialize each chunk
 
-        // Serialize each chunk
+		// Update global information
 
-        // Update global information
+		return -1;
+	}
 
-        return -1;
-    }
+	private Set<Chunk<T>> computeChunk() {
+		// Compute
+		int totalSize = memorySet.size();
 
+		// Compute chunk size
+		int chunkSize = totalSize / 10;
 
-    private List<Chunk> computeChunk() {
-        // Compute
-        int totalSize = memorySet.size();
+		// Compute boundaries
 
-        // Compute chunk size
-        int chunkSize = totalSize / 10;
+		return null;
+	}
 
-        // Compute boundaries
-
-        return null;
-    }
-
-    private class CONFIG {
-        public static final int INITIAL_OFFSET = 10;
-    }
-
-    private class Chunk {
-
-    }
+	private class CONFIG {
+		public static final int INITIAL_OFFSET = 10;
+	}
 }
-
-
-
-
